a8ad253dc9f9068f3003a241f85e219981f15ee5
Zahid Gul <zgull429@gmail.com>
2025-05-14 16:51:19 +0500

    Add project files.

---
+++ VideoGamecs.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiDotNetCore9
+{
+    public class VideoGamecs
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Platform { get; set; }
+        public string? Developer { get; set; }
+        public string Publisher { get; set; }
+    }
+}
